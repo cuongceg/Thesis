098b2f77591ace2f86d5e8478d0d5f31f2e2cbe5
Nick Bakkegaard <nickbakkegaard@gmail.com>
2018-03-01 10:31:04 +0100

    add list class

---
+++ list.cpp
@@ -0,0 +1,15 @@
+template <class E> class list{
+	public:
+		//Standard list operations
+		void insert(E element);
+		void delete();
+		concatenate();
+		split();
+		findPrevious();
+		findNext();
+		search();
+
+		//Priority queue operations
+		updatePriority();
+		findMin();
+};
