27b580481dc7db981d3855b631ad0ba30ec8cc4c
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-04-15 09:44:00 +0700

    move struct to draw.hpp

--- main.cpp
+++ main.cpp
@@ -8,58 +8,13 @@
 #include <tuple> //get<n> make_tuple
 #include <chrono>
 
-
+#include "draw.hpp"
+#include "naive.cpp"
 //So we don't need to write std:: everywhere
 using namespace std;
 
-struct {
-	bool printGraph = false;
-	bool drawRoute = true;
-	int printLevel = 0;
-	size_t k = 0;
-} config;
-
 //point type for holding a coordinate 
 void setMinMax(double x, double y);
-struct point {
-	double x;
-	double y;
-	point(double x,double y){
-		this->x = x;
-		this->y = y;
-		setMinMax( x, y);
-	}
-
-	point(){
-
-	}
-	bool operator==(const point &other) const {
-		return (this->x==other.x && this->y==other.y);
-	}
-};
-//Type used in the priority queue in the dijkstra function
-typedef std::pair<double,std::pair<int,int> > pq_pair;
-
-//linesegment type holding two points
-struct lineSegment {
-	point p;
-	point q;
-	lineSegment(point p,point q){
-		this->p = p;
-		this->q = q;
-	};
-	lineSegment(){
-
-	}
-	bool operator==(const lineSegment &other) const {
-		return (this->p==other.p && this->q==other.q) ||
-		(this->p==other.q && this->q==other.p);
-  }
-};
-
-#include "draw.cpp"
-#include "naive.cpp"
-
 
 void setConfig(int argc, const char* argv[]){
 	for(int i = 1 ; i < argc ; i++ ){
